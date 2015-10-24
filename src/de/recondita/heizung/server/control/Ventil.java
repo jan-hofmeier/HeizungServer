a98d3164eb972707974391df2d75fbb4be104e89
jan <jan.hofmeier@yahoo.de>
2015-10-24 23:33:22 +0200

    Nicht Serilisierbar

--- src/de/recondita/heizung/server/control/Ventil.java
+++ src/de/recondita/heizung/server/control/Ventil.java
@@ -1,11 +1,7 @@
 package de.recondita.heizung.server.control;
 
-import java.io.Serializable;
-
-public class Ventil implements Serializable{
-
-	private static final long serialVersionUID = -1925414901838822683L;
-
+public class Ventil{
+	
 	public Ventil() {
 		// TODO Auto-generated constructor stub
 	}
--- src/de/recondita/heizung/server/control/Zeitplan.java
+++ src/de/recondita/heizung/server/control/Zeitplan.java
@@ -1,10 +1,6 @@
 package de.recondita.heizung.server.control;
 
-import java.io.Serializable;
-
-public class Zeitplan implements Serializable{
-	
-	private static final long serialVersionUID = 8115805587974009065L;
+public class Zeitplan{
 
 	public Zeitplan() {
 		// TODO Auto-generated constructor stub
