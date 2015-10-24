c7b35a9bc7664edc30089861f8460cc2e6b277cc
jan <jan.hofmeier@yahoo.de>
2015-10-24 23:27:22 +0200

    Project

---
+++ src/de/recondita/heizung/server/control/Ventil.java
@@ -0,0 +1,13 @@
+package de.recondita.heizung.server.control;
+
+import java.io.Serializable;
+
+public class Ventil implements Serializable{
+
+	private static final long serialVersionUID = -1925414901838822683L;
+
+	public Ventil() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/de/recondita/heizung/server/control/Zeitplan.java
@@ -0,0 +1,13 @@
+package de.recondita.heizung.server.control;
+
+import java.io.Serializable;
+
+public class Zeitplan implements Serializable{
+	
+	private static final long serialVersionUID = 8115805587974009065L;
+
+	public Zeitplan() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
