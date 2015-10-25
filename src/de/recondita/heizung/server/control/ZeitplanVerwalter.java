29a85bcff6a53f3c66c4ba2ede3b1f6bbbbdf91f
Jan Hofmeier <jan.hofmeier@recondita.de>
2015-10-25 13:25:58 +0100

    Zeitplanverwalter

---
+++ src/de/recondita/heizung/server/control/ZeitplanVerwalter.java
@@ -0,0 +1,9 @@
+package de.recondita.heizung.server.control;
+
+import java.util.ArrayList;
+
+public class ZeitplanVerwalter {
+	private ArrayList<Ventil> ventile;
+	private ArrayList<Zeitplan> plane;
+	
+}
