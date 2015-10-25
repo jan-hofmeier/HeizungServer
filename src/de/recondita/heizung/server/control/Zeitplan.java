d681ee57d5bfd10754943f2996de4323dd4c3792
Jan Hofmeier <jan.hofmeier@recondita.de>
2015-10-25 03:22:16 +0100

    müde

--- src/de/recondita/heizung/server/control/Zeitplan.java
+++ src/de/recondita/heizung/server/control/Zeitplan.java
@@ -1,9 +1,60 @@
 package de.recondita.heizung.server.control;
 
-public class Zeitplan{
+import java.io.Closeable;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
-	public Zeitplan() {
-		// TODO Auto-generated constructor stub
+import sun.util.calendar.Gregorian;
+
+public class Zeitplan implements Closeable {
+
+	private LocalTime[][] plan;
+	private ArrayList<Ventil> ventile = new ArrayList<Ventil>();
+	private Timer timer = new Timer();
+
+	public Zeitplan(LocalTime[][] plan, Ventil[] ventile) {
+		this.plan = plan;
+		for (Ventil ventil : ventile)
+			addVentil(ventil);
 	}
 
+	public void addVentil(Ventil ventil) {
+		synchronized (ventile) {
+			ventile.add(ventil);
+		}
+	}
+
+	public synchronized void setPlan(LocalTime[][] plan) {
+		this.plan = plan.clone();
+	}
+
+	public Ventil[] getVentile() {
+		synchronized (ventile) {
+			return ventile.toArray(new Ventil[ventile.size()]);
+		}
+	}
+
+	public synchronized void start() {
+		run();
+	}
+
+	@Override
+	public void close() {
+		timer.cancel();
+	}
+
+	private void run() {
+		int day=GregorianCalendar.DAY_OF_WEEK;
+		LocalTime now = LocalTime.now();
+		
+		//TODO
+		int i=0;
+		while(plan[day][i].compareTo(now)<0&& i<plan[day].length)
+			i++;
+			
+		
+	}
 }
