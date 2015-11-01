1dc2ad9e5404e82da539428980c488e2f4bb90ef
jan <jan.hofmeier@yahoo.de>
2015-11-01 22:55:48 +0100

    Warten

--- src/de/recondita/heizung/server/control/Ventilverwalter.java
+++ src/de/recondita/heizung/server/control/Ventilverwalter.java
@@ -68,6 +68,11 @@ public class Ventilverwalter {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		try (FileWriter out = new FileWriter("/sys/class/gpio/gpio" + pin + "/direction");) {
 			out.write("out");
 		} 
