4dbd4934d14ef61e71c34af758287948e707fa70
jan <jan.hofmeier@yahoo.de>
2015-11-01 02:49:44 +0100

    XMLLoader weiter gemacht

--- src/de/recondita/heizung/xml/XMLLoader.java
+++ src/de/recondita/heizung/xml/XMLLoader.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -28,16 +29,32 @@ import de.recondita.heizung.server.control.Zeitplan;
 
 public class XMLLoader {
 
-	private XPath xPath = XPathFactory.newInstance().newXPath();
+	private final XPath xPath = XPathFactory.newInstance().newXPath();
 	private DocumentBuilder builder;
 	private File configdir;
+	private final XPathExpression zeitplaenePath;
+	private final XPathExpression tagesPlaenePath;
+	private final XPathExpression tagesPlanPath;
+	private final XPathExpression namePath;
+	private final XPathExpression zeitplanePath;
+	private final XPathExpression gpioPath;
+	private final XPathExpression schaltpunktePath;
 
-	public XMLLoader(File configdir) throws ParserConfigurationException {
+	public XMLLoader(File configdir) throws ParserConfigurationException, XPathExpressionException {
 		builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		zeitplaenePath = xPath.compile("/zeitplaene");
+		tagesPlaenePath = xPath.compile("tagesplaene");
+		tagesPlanPath = xPath.compile("tagesplane");
+		namePath = xPath.compile("name");
+		zeitplanePath = xPath.compile("/ventile");
+		gpioPath = xPath.compile("gpio");
+		schaltpunktePath = xPath.compile("schaltpunkte");
+		;
 		this.configdir = configdir;
 	}
 
-	public void loadVentile(Ventilverwalter ventilverwalter) throws FileNotFoundException, IOException, XPathExpressionException, SAXException {
+	public void loadVentile(Ventilverwalter ventilverwalter)
+			throws FileNotFoundException, IOException, XPathExpressionException, SAXException {
 		try (FileInputStream fis = new FileInputStream(
 				configdir.getAbsolutePath() + File.pathSeparator + "Ventile.xml");) {
 			loadVentile(new InputStreamReader(fis), ventilverwalter);
@@ -47,11 +64,8 @@ public class XMLLoader {
 	public void loadVentile(Reader xml, Ventilverwalter ventilverwalter)
 			throws XPathExpressionException, SAXException, IOException {
 		Document xmlDocument = builder.parse(new InputSource(xml));
-		XPathExpression root = xPath.compile("/ventile");
-		XPathExpression gpioPath = xPath.compile("gpio");
-		XPathExpression namePath = xPath.compile("name");
 
-		NodeList ventile = (NodeList) root.evaluate(xmlDocument, XPathConstants.NODESET);
+		NodeList ventile = (NodeList) zeitplanePath.evaluate(xmlDocument, XPathConstants.NODESET);
 
 		for (int i = 0; i < ventile.getLength(); i++) {
 			Node v = ventile.item(i);
@@ -59,22 +73,36 @@ public class XMLLoader {
 		}
 	}
 
-	
-	public ArrayList<Zeitplan> loadZeitplaene(Reader xml, Ventilverwalter ventilverwalter) throws SAXException, IOException, XPathExpressionException {
+	public ArrayList<Zeitplan> loadZeitplaene(Reader xml, Ventilverwalter ventilverwalter)
+			throws SAXException, IOException, XPathExpressionException {
 		Document xmlDocument = builder.parse(new InputSource(xml));
-		XPathExpression root = xPath.compile("/zeitplane");
-		XPathExpression gpioPath = xPath.compile("schaltpunkte");
-		XPathExpression namePath = xPath.compile("name");
-		
-		NodeList zeitPlaene = (NodeList) root.evaluate(xmlDocument, XPathConstants.NODESET);
-		
-		ArrayList<Zeitplan> ret= new ArrayList<Zeitplan>(zeitPlaene.getLength());
-		
-		for(int i=0; i<zeitPlaene.getLength(); i++)
-		{
-			//TODO
+		NodeList zeitPlaene = (NodeList) zeitplaenePath.evaluate(xmlDocument, XPathConstants.NODESET);
+
+		ArrayList<Zeitplan> ret = new ArrayList<Zeitplan>(zeitPlaene.getLength());
+
+		for (int i = 0; i < zeitPlaene.getLength(); i++) {
+			Node zeitplanNode = zeitPlaene.item(i);
+			int id = Integer.parseInt(zeitplanNode.getAttributes().getNamedItem("id").getNodeValue());
+			String name = namePath.evaluate(zeitplanNode);
+			Node tagesplaene = (Node) tagesPlaenePath.evaluate(zeitplanNode, XPathConstants.NODE);
+			LocalTime[][] plan = evaluateZeitplan(tagesplaene);
+			ret.add(new Zeitplan(id, name, plan));
 		}
-		
+		return ret;
+	}
+
+	private LocalTime[][] evaluateZeitplan(Node tagesplaene) throws XPathExpressionException {
+		NodeList tagNodes = (NodeList) tagesPlanPath.evaluate(tagesplaene, XPathConstants.NODESET);
+		LocalTime[][] ret = new LocalTime[7][];
+		for (int i = 0; i < tagNodes.getLength(); i++) {
+			Node tagNode=tagNodes.item(i);
+			
+		}
+		return ret;
+	}
+	
+	private LocalTime[] evaluateSchaltpunkte(Node schaltpunkte)
+	{
 		return null;
 	}
 
