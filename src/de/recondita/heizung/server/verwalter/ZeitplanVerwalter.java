55168bc5759f16cd1a26549b89c8b6ba2c6ab0a4
jan <jan.hofmeier@yahoo.de>
2015-11-01 15:36:59 +0100

    Zeitplanverwalter

--- src/de/recondita/heizung/server/verwalter/ZeitplanVerwalter.java
+++ src/de/recondita/heizung/server/verwalter/ZeitplanVerwalter.java
@@ -1,22 +1,37 @@
 package de.recondita.heizung.server.verwalter;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import de.recondita.heizung.server.control.Ventil;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+import de.recondita.heizung.server.control.Ventilverwalter;
 import de.recondita.heizung.server.control.Zeitplan;
+import de.recondita.heizung.xml.XMLLoader;
+import de.recondita.heizung.xml.XMLLoader.PunktOrderException;
 
 public class ZeitplanVerwalter implements Closeable {
-	private ArrayList<Ventil> ventile;
-	private ArrayList<Zeitplan> zeitPlaene = new ArrayList<Zeitplan>();
+	private Ventilverwalter ventile;
+	private XMLLoader configurationLoader;
+	private ArrayList<Zeitplan> zeitPlaene;
 
-	public ZeitplanVerwalter(ArrayList<Ventil> ventile) {
-		this.ventile = ventile;
-	}
 
-	public ZeitplanVerwalter() {
-		this(new ArrayList<Ventil>());
+	public ZeitplanVerwalter(Ventilverwalter ventilverwalter, XMLLoader configurationLoader)
+			throws FileNotFoundException, XPathExpressionException, IOException, SAXException, PunktOrderException {
+		this.ventile=ventilverwalter;
+		this.configurationLoader=configurationLoader;
+		this.configurationLoader.loadVentile(ventile);
+		this.zeitPlaene=this.configurationLoader.loadZeitplaene(ventilverwalter);
+		for(Zeitplan z:zeitPlaene)
+		{
+			z.start();
+		}
 	}
 
 	@Override
@@ -26,6 +41,11 @@ public class ZeitplanVerwalter implements Closeable {
 				z.close();
 		}
 	}
-	
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws FileNotFoundException, XPathExpressionException, IOException, SAXException, PunktOrderException, ParserConfigurationException
+	{
+		new ZeitplanVerwalter(Ventilverwalter.getInstance(),new XMLLoader(new File("config")));
+	}
 
 }
