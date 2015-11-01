43cc298bfafdf6ac546af52cf4e456e93a3f45e0
jan <jan.hofmeier@yahoo.de>
2015-11-01 02:03:45 +0100

    XML-Loader angefangen

--- src/de/recondita/heizung/xml/XMLLoader.java
+++ src/de/recondita/heizung/xml/XMLLoader.java
@@ -1,7 +1,12 @@
 package de.recondita.heizung.xml;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,29 +24,58 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import de.recondita.heizung.server.control.Ventilverwalter;
+import de.recondita.heizung.server.control.Zeitplan;
 
 public class XMLLoader {
 
 	private XPath xPath = XPathFactory.newInstance().newXPath();
 	private DocumentBuilder builder;
+	private File configdir;
 
-	public XMLLoader() throws ParserConfigurationException {
+	public XMLLoader(File configdir) throws ParserConfigurationException {
 		builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		this.configdir = configdir;
 	}
 
-	public void loadVentile(String xml, Ventilverwalter ventilverwalter) throws XPathExpressionException, SAXException, IOException {
-		StringReader stringReader = new StringReader(xml);
-		Document xmlDocument = builder.parse(new InputSource(stringReader));
+	public void loadVentile(Ventilverwalter ventilverwalter) throws FileNotFoundException, IOException, XPathExpressionException, SAXException {
+		try (FileInputStream fis = new FileInputStream(
+				configdir.getAbsolutePath() + File.pathSeparator + "Ventile.xml");) {
+			loadVentile(new InputStreamReader(fis), ventilverwalter);
+		}
+	}
+
+	public void loadVentile(Reader xml, Ventilverwalter ventilverwalter)
+			throws XPathExpressionException, SAXException, IOException {
+		Document xmlDocument = builder.parse(new InputSource(xml));
 		XPathExpression root = xPath.compile("/ventile");
 		XPathExpression gpioPath = xPath.compile("gpio");
 		XPathExpression namePath = xPath.compile("name");
-		
+
 		NodeList ventile = (NodeList) root.evaluate(xmlDocument, XPathConstants.NODESET);
 
 		for (int i = 0; i < ventile.getLength(); i++) {
 			Node v = ventile.item(i);
 			ventilverwalter.createVentil(Integer.parseInt(gpioPath.evaluate(v)), namePath.evaluate(v));
 		}
+	}
+
+	
+	public ArrayList<Zeitplan> loadZeitplaene(Reader xml, Ventilverwalter ventilverwalter) throws SAXException, IOException, XPathExpressionException {
+		Document xmlDocument = builder.parse(new InputSource(xml));
+		XPathExpression root = xPath.compile("/zeitplane");
+		XPathExpression gpioPath = xPath.compile("schaltpunkte");
+		XPathExpression namePath = xPath.compile("name");
+		
+		NodeList zeitPlaene = (NodeList) root.evaluate(xmlDocument, XPathConstants.NODESET);
+		
+		ArrayList<Zeitplan> ret= new ArrayList<Zeitplan>(zeitPlaene.getLength());
+		
+		for(int i=0; i<zeitPlaene.getLength(); i++)
+		{
+			//TODO
+		}
+		
+		return null;
 	}
 
 }
