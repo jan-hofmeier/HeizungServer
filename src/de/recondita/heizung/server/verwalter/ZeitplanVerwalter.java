db1965277704879aac8a00ab31661b50f72f438b
Jan Hofmeier <jan.hofmeier@gmail.com>
2020-12-26 23:44:44 +0100

    0.1 -> 0.12 to allow for FP inprecision

--- src/de/recondita/heizung/server/verwalter/ZeitplanVerwalter.java
+++ src/de/recondita/heizung/server/verwalter/ZeitplanVerwalter.java
@@ -125,7 +125,7 @@ public class ZeitplanVerwalter implements Closeable {
 					if (ventil.getPlanOn() && now - ventil.getLastChanged() > 15 * 60000
 							|| !ventil.getPlanOn() && now - ventil.getLastChanged() > 30 * 60000 || targetChanged) {
 
-						if (Math.abs(targetTemp - currentTemp) <= 0.1)
+						if (Math.abs(targetTemp - currentTemp) < 0.12)
 							ventil.setPlanOn(!ventil.getPlanOn());
 						else
 							ventil.setPlanOn(targetTemp > currentTemp);
